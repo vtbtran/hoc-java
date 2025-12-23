fdabd9c6d8d753d61204104a8b74ccc06669b19b
vtbtran <vothibaotran01012005@gmail.com>
2025-12-23 20:55:45 +0700

    import thu vien scanner, cho nguoi dung nhap vao gia tri

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,20 @@
 package hoc_java;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World và bắt đầu học ");
-		System.out.println("Hello World và bắt đầu học ");
-		System.out.println("Hello World và bắt đầu học ");
+//		int age = 20;
+//		String name = "Bao Tran";
+//		System.out.println("my age is: " + age + ", name is: " + name);
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is: ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is: ");
+		int age = scanner.nextInt();
+		System.out.println("My name is: " + name + ",age: " + age);
+		scanner.close();
 	}
 
 }
