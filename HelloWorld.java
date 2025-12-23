74c2a236dff00d7113a034c1d6b58943981357f0
vtbtran <vothibaotran01012005@gmail.com>
2025-12-23 21:12:27 +0700

    cac phep tinh toan hoc

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+
 //		int age = 20;
 //		String name = "Bao Tran";
 //		System.out.println("my age is: " + age + ", name is: " + name);
 
+//		int a = 10;
+//		int b = 6;
+//
+//		int c = Math.min(10, 6);
+//		System.out.println("Min = " + c);
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Your name is: ");
-		String name = scanner.nextLine();
-		System.out.println("Your age is: ");
-		int age = scanner.nextInt();
-		System.out.println("My name is: " + name + ",age: " + age);
+//		System.out.println("First number: " + Math.pow(2, 3)); // pow tinh so mu
+		System.out.println("First number: " + Math.sqrt(49)); // pow tinh so mu
+		int firstNumber = scanner.nextInt();
+		System.out.println("Second number: ");
+		int secondNumber = scanner.nextInt();
+		System.out.println("Min = " + Math.min(firstNumber, secondNumber));
 		scanner.close();
 	}
 
